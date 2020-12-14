21bc290342571ae7faac38f7b425c255f807180d
Austin Zheng <austin.zheng@ringcentral.com>
2020-12-14 14:07:53 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Austin Zheng on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
